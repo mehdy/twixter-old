199007eab7ad42644153d8b9fe6317ec28ad1a4d
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2020-09-19 21:46:48 +0430

    Change Logger.As to accept string instead of int
    Signed-off-by: Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>

--- pkg/entities/log.go
+++ pkg/entities/log.go
@@ -2,8 +2,8 @@ package entities
 
 // Logger defines logging behaviour.
 type Logger interface {
-	// As sets the level of log. From 0 to 4 for Debug, Info, Warn, Error and Fatal respectively.
-	As(level int8) Logger
+	// As sets the level of log. "D", "I", "W", "E", "F" for Debug, Info, Warn, Error and Fatal respectively.
+	As(level string) Logger
 	// WithField adds a pair of key/value to the log entry.
 	WithField(key string, value interface{}) Logger
 	// WithError adds an error value to the log entry.
