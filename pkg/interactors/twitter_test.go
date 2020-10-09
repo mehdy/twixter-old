31cb79f2a621606f41db17ef97b2c5bfe1d589ea
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2020-10-09 22:29:53 +0330

    Add tests for UpdateNetwork of Twitter
    Signed-off-by: Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>

--- pkg/interactors/twitter_test.go
+++ pkg/interactors/twitter_test.go
@@ -1,4 +1,4 @@
-// nolint: goerr113, funlen, dupl, goconst
+// nolint: goerr113, funlen, dupl, goconst, scopelint, gocognit
 package interactors_test
 
 import (
@@ -237,4 +237,136 @@ func TestTiwtterIntractor(suiteT *testing.T) {
 			t.Fail()
 		}
 	})
+
+	updateNetowrkTestTable := []struct {
+		title      string
+		suffix     string
+		followings bool
+		followers  bool
+		depth      int
+	}{
+		{"UpdateNetworkOfFollowingsDepth1Successfully", "A", true, false, 1},
+		{"UpdateNetworkOfFollowingsDepth2Successfully", "A", true, false, 2},
+		{"UpdateNetworkOfFollowingsDepth3Successfully", "A", true, false, 3},
+		{"UpdateNetworkOfFollowersDepth1Successfully", "B", false, true, 1},
+		{"UpdateNetworkOfFollowersDepth2Successfully", "B", false, true, 2},
+		{"UpdateNetworkOfFollowersDepth3Successfully", "B", false, true, 3},
+		{"UpdateNetworkOfBothDepth1Successfully", "C", true, true, 1},
+		{"UpdateNetworkOfBothDepth2Successfully", "C", true, true, 2},
+		{"UpdateNetworkOfBothDepth3Successfully", "C", true, true, 3},
+	}
+	for _, test := range updateNetowrkTestTable {
+		run(test.title, func(
+			t *testing.T,
+			twitter entities.TwitterInteractor,
+			twitterAPIMock *mocks.MockTwitterAPI,
+			storeMock *mocks.MockStore,
+		) {
+			profile := mocks.GenerateProfile(test.suffix)
+
+			if test.followings {
+				followingsMap := map[*entities.TwitterProfile][]*entities.TwitterProfile{
+					profile: mocks.GenerateProfileBatches(10, 1)[0],
+				}
+				for i := 0; i < test.depth; i++ {
+					for p, followings := range followingsMap {
+						setupFollowings(p, [][]*entities.TwitterProfile{followings}, storeMock, twitterAPIMock)
+						storeMock.EXPECT().GetFollowings(gomock.Eq(p.Username)).Return(followingsMap[p], nil)
+					}
+
+					profiles := []*entities.TwitterProfile{}
+					for _, followings := range followingsMap {
+						profiles = append(profiles, followings...)
+					}
+
+					followingsMap = map[*entities.TwitterProfile][]*entities.TwitterProfile{}
+					for _, p := range profiles {
+						followingsMap[p] = mocks.GenerateProfileBatches(10, 1)[0]
+					}
+				}
+			}
+			if test.followers {
+				followersMap := map[*entities.TwitterProfile][]*entities.TwitterProfile{
+					profile: mocks.GenerateProfileBatches(10, 1)[0],
+				}
+				for i := 0; i < test.depth; i++ {
+					for p, followers := range followersMap {
+						setupFollowers(p, [][]*entities.TwitterProfile{followers}, storeMock, twitterAPIMock)
+						storeMock.EXPECT().GetFollowers(gomock.Eq(p.Username)).Return(followersMap[p], nil)
+					}
+
+					profiles := []*entities.TwitterProfile{}
+					for _, followers := range followersMap {
+						profiles = append(profiles, followers...)
+					}
+
+					followersMap = map[*entities.TwitterProfile][]*entities.TwitterProfile{}
+					for _, p := range profiles {
+						followersMap[p] = mocks.GenerateProfileBatches(10, 1)[0]
+					}
+				}
+			}
+			twitter.UpdateNetwork(profile.Username, test.followings, test.followers, test.depth)
+		})
+	}
+}
+
+func setupFollowings(
+	profile *entities.TwitterProfile,
+	profiles [][]*entities.TwitterProfile,
+	storeMock *mocks.MockStore,
+	twitterAPIMock *mocks.MockTwitterAPI,
+) {
+	storeMock.EXPECT().GetProfile(gomock.Eq(profile.Username)).Return(profile, nil)
+
+	storeCalls := []*gomock.Call{}
+	for _, profileBatch := range profiles {
+		storeCalls = append(storeCalls,
+			storeMock.EXPECT().SaveProfiles(gomock.Eq(profileBatch)),
+			storeMock.EXPECT().AddFollowings(gomock.Eq(profile), gomock.Eq(profileBatch)),
+		)
+	}
+
+	gomock.InOrder(storeCalls...)
+
+	followingsCh := make(chan []*entities.TwitterProfile)
+	twitterAPIMock.EXPECT().Followings(gomock.Eq(profile.Username)).Return(followingsCh, nil)
+
+	go func() {
+		for _, profileBatch := range profiles {
+			followingsCh <- profileBatch
+		}
+
+		close(followingsCh)
+	}()
+}
+
+func setupFollowers(
+	profile *entities.TwitterProfile,
+	profiles [][]*entities.TwitterProfile,
+	storeMock *mocks.MockStore,
+	twitterAPIMock *mocks.MockTwitterAPI,
+) {
+	storeMock.EXPECT().GetProfile(gomock.Eq(profile.Username)).Return(profile, nil)
+
+	storeCalls := []*gomock.Call{}
+	for _, profileBatch := range profiles {
+		storeCalls = append(storeCalls,
+			storeMock.EXPECT().SaveProfiles(gomock.Eq(profileBatch)),
+			storeMock.EXPECT().AddFollowers(gomock.Eq(profile), gomock.Eq(profileBatch)),
+		)
+	}
+
+	gomock.InOrder(storeCalls...)
+
+	followingsCh := make(chan []*entities.TwitterProfile)
+	twitterAPIMock.EXPECT().Followers(gomock.Eq(profile.Username)).Return(followingsCh, nil)
+
+	go func() {
+		for _, profileBatch := range profiles {
+			followingsCh <- profileBatch
+		}
+
+		close(followingsCh)
+	}()
 }
