04bbd83fdb97830eef2cf7cfddf0b2725bbedd2b
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2020-10-14 22:16:32 +0330

    Add Profile method implementation of Twitter API
    Signed-off-by: Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>

--- pkg/services/twitter/twitter.go
+++ pkg/services/twitter/twitter.go
@@ -32,3 +32,60 @@ func NewTwitter(config entities.ConfigGetter, logger entities.Logger) *Twitter {
 		logger: logger,
 	}
 }
+
+func (t *Twitter) Profile(username string) (*entities.TwitterProfile, error) {
+	user, resp, err := t.api.Users.Show(&twitter.UserShowParams{ScreenName: username})
+	if err != nil {
+		t.logger.As("E").
+			WithError(err).
+			WithField("username", username).
+			Logf("Failed to fetch profile from twitter API")
+
+		return nil, newError(err, "failed to fetch profile from twitter API")
+	}
+	defer resp.Body.Close()
+
+	return t.asTwitterProfile(*user), nil
+}
+
+func (t *Twitter) asTwitterProfile(user twitter.User) *entities.TwitterProfile {
+	joinedAt, err := time.Parse(time.RubyDate, user.CreatedAt)
+	if err != nil {
+		t.logger.As("W").WithError(err).Logf("Failed to parse joinedAt time")
+	}
+
+	jsonEntites, err := json.Marshal(user.Entities)
+	if err != nil {
+		t.logger.As("W").WithError(err).Logf("Failed to convert user.Entites to JSON")
+	}
+
+	var ent map[string]interface{}
+
+	err = json.Unmarshal(jsonEntites, &ent)
+	if err != nil {
+		t.logger.As("W").WithError(err).Logf("Failed to convert user.Entites to JSON")
+	}
+
+	return &entities.TwitterProfile{
+		TwitterID:           user.IDStr,
+		Name:                user.Name,
+		Username:            user.ScreenName,
+		Location:            user.Location,
+		Bio:                 user.Description,
+		URL:                 user.URL,
+		Email:               user.Email,
+		ProfileBannerURL:    user.ProfileBannerURL,
+		ProfileImageURL:     user.ProfileImageURLHttps,
+		Verified:            user.Verified,
+		Protected:           user.Protected,
+		DefaultProfile:      user.DefaultProfile,
+		DefaultProfileImage: user.DefaultProfileImage,
+		FollowersCount:      user.FollowersCount,
+		FollowingsCount:     user.FriendsCount,
+		FavouritesCount:     user.FavouritesCount,
+		ListedCount:         user.ListedCount,
+		TweetsCount:         user.StatusesCount,
+		Entities:            ent,
+		JoinedAt:            joinedAt,
+	}
+}
