a2bfe95a8ab144e9bc54551df31b357b6755aa92
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2020-10-28 21:43:11 +0330

    Add GetTopFollowersByFollowers implementation
    Signed-off-by: Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>

--- pkg/interactors/twitter.go
+++ pkg/interactors/twitter.go
@@ -229,7 +229,16 @@ func (t *Twitter) GetTopFollowingsByFollowers(username string, limit int) ([]*en
 }
 
 func (t *Twitter) GetTopFollowersByFollowers(username string, limit int) ([]*entities.TwitterProfile, error) {
-	panic("not implemented") // TODO: Implement
+	profiles, err := t.store.GetTopFollowersByFollowers(username, limit)
+	if err != nil {
+		t.logger.As("E").
+			WithError(err).
+			WithField("username", username).
+			WithField("limit", limit).
+			Logf("Failed to get top followers by followers")
+		return nil, newError(err, "failed to get top followers by followers")
+	}
+	return profiles, nil
 }
 
 func (t *Twitter) GetTopFollowedByFollowings(username string, followed bool, limit int) (
